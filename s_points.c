ea1e7ec363b5ac12c0d7d5a5c3d97cb49e8557e5
George Makris <g.makris96@yahoo.com>
2022-02-03 23:10:32 +0200

    Array loop through

--- s_points.c
+++ s_points.c
@@ -3,5 +3,11 @@
 short int array[3][3] = {1,2,3,4,5,6,7,8,9};
 
 int main(){
+    for(int row = 0; row < 3; row++){ //looping through the array
+        for(int col = 0; col < 3; col++){
+            printf(" %d ",array[row][col]);
+        }
+        printf("\n");
+    }
     return 0;
 }
